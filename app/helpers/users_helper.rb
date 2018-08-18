89a25d37b40317b55b7f5b2cebf0f6d9b9f484a1
Linh Tran <linhttd.bk@gmail.com>
2018-08-18 09:50:23 +0000

    static page 3

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,5 @@
 module UsersHelper
+  def avatar_url(user)
+    user.images.last&.file&.url || 'https://previews.123rf.com/images/stalkerstudent/stalkerstudent1601/stalkerstudent160101173/50961996-user-icon-vector-flat-design-style-eps-10.jpg'
+  end
 end
